3beb3aed100f0def9922f86ec507b281b170debf
Alisa! <122760610+alisam707@users.noreply.github.com>
2023-12-06 22:30:50 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,50 +9,50 @@
 using namespace std;
 
 int main() {
-    ifstream inFile;
-    ofstream outFile("output.txt");
-    string filename;
-    string line;
+  ifstream inFile;
+  ofstream outFile("output.txt");
+  string filename;
+  string line;
 
-    cout << "Enter test case name (ex: test.txt): ";
-    cin >> filename;
+  cout << "Enter test case name (ex: test.txt): ";
+  cin >> filename;
 
-    inFile.open(filename.c_str());
+  inFile.open(filename.c_str());
 
-    // Make sure file is opened
-    if (!inFile.is_open()) {
-        cerr << "Failed to open the file.\n";
-        cerr << "Please enter correct file.\n";
-        exit(1);
-    } /*else {
-      cout << "SOURCE CODE:\n\n";
-      outFile << "SOURCE CODE:\n\n";
-      while (getline(inFile, line)) {
-        cout << line << endl;
-        outFile << line << endl;
-      }
+  // Make sure file is opened
+  if (!inFile.is_open()) {
+    cerr << "Failed to open the file.\n";
+    cerr << "Please enter correct file.\n";
+    exit(1);
+  } /*else {
+    cout << "SOURCE CODE:\n\n";
+    outFile << "SOURCE CODE:\n\n";
+    while (getline(inFile, line)) {
+      cout << line << endl;
+      outFile << line << endl;
     }
+  }
 
-    cout << "\n\nOUTPUT:\n";
-    cout << endl;
+  cout << "\n\nOUTPUT:\n";
+  cout << endl;
 
-    outFile << "\n\nOUTPUT:\n";
-    outFile << endl;
-  */
-    Parser check;
+  outFile << "\n\nOUTPUT:\n";
+  outFile << endl;
+*/
+  Parser check;
 
-    inFile.clear();
-    inFile.seekg(0);
+  inFile.clear();
+  inFile.seekg(0);
 
-    // run parser
-    check.RAT23F(inFile, outFile);
-    inFile.close();
+  // run parser
+  check.RAT23F(inFile, outFile);
+  inFile.close();
 
-    cout << endl;
-    outFile << endl;
+  cout << endl;
+  outFile << endl;
 
-    outFile.close();
-    cout << "see output.txt for results" << endl;
+  outFile.close();
+  cout << "see output.txt for results" << endl;
 
-    return 0;
+  return 0;
 }
